e8370bad5c4b5161f0da3fab7734933fdb932b87
Archimine <45541080+Archimine@users.noreply.github.com>
2022-02-19 17:46:38 -0500

    Add files via upload

---
+++ templates/final.php
@@ -0,0 +1,28 @@
+    <footer class="container-fluid final letras-final">
+      <div class="row secundario padding-final">
+              <div class="contenedor-final">
+                <p class="textos">
+                  >Bryan Madrid
+                </p>
+                <p class="textos">
+                  >Luis Loor
+                </p>
+                <p class="textos">
+                  >Mateo Murillo
+                </p>
+              </div>
+
+              <p class="textos" class = "row">
+                Proyecto: Escuela de estudiantes de artes marciales
+              </p>
+      </div>
+      <div class="row secundario-oscuro padding-final">
+      <p>
+          &copy; Trabajo de Ingenieria del software
+        </p>
+      </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </footer>
+  </body>
+</html>
